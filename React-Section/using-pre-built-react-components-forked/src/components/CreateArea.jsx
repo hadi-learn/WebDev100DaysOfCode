1c0a4c6f44e05d1e57fdaf1eec5f8108bddbfcb9
Hadi <hadilearn01@gmail.com>
2023-02-12 20:20:37 +0700

    finish react section

--- React-Section/using-pre-built-react-components-forked/src/components/CreateArea.jsx
+++ React-Section/using-pre-built-react-components-forked/src/components/CreateArea.jsx
@@ -1,10 +1,15 @@
-import React, { useState } from "react";
+import React, { useState } from "react"
+import AddIcon from '@mui/icons-material/Add'
+import Fab from '@mui/material/Fab'
+import Zoom from '@mui/material/Zoom'
 
 function CreateArea(props) {
   const [note, setNote] = useState({
     title: "",
     content: ""
   });
+
+  const [isExpanded, setExpanded] = useState(false)
 
   function handleChange(event) {
     const { name, value } = event.target;
@@ -26,23 +31,34 @@ function CreateArea(props) {
     event.preventDefault();
   }
 
+  function inputClickHandler() {
+    setExpanded(true) 
+  }
+
   return (
     <div>
       <form className="create-note">
-        <input
+        {isExpanded? <input
           name="title"
           onChange={handleChange}
           value={note.title}
           placeholder="Title"
-        />
+        /> : null}
         <textarea
           name="content"
+          placeholder="Take a note..."
+          rows={isExpanded ? "3" : "1"}
+          onClick={inputClickHandler}
           onChange={handleChange}
           value={note.content}
-          placeholder="Take a note..."
-          rows="3"
+          p
         />
-        <button onClick={submitNote}>Add</button>
+        <Zoom in={isExpanded}>
+          <Fab onClick={submitNote}>
+            <AddIcon 
+            />
+          </Fab>
+        </Zoom>
       </form>
     </div>
   );
